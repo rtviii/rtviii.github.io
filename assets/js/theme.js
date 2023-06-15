68ca1a3eab27ddc9f7fc4b87b300ef5e411c7a6a
rtviii <34076599+rtviii@users.noreply.github.com>
2023-06-15 11:47:55 +0000

    Deploying to gh-pages from @ rtviii/rtviii.github.io@1af2fe680cc5b96b1d200739e8989ea472f49b1e 🚀

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -75,7 +75,7 @@ let transTheme = () => {
 let initTheme = (theme) => {
   if (theme == null || theme == "null") {
     const userPref = window.matchMedia;
-    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+    if (userPref && userPref("(prefers-color-scheme: white)").matches) {
       theme = "dark";
     }
   }
